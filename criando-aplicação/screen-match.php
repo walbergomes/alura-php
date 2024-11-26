77768db09462f4b1ca1a5abc426c63699c5bcdf6
Walber <walberg.s48@gmail.com>
2024-11-26 18:07:55 -0300

    trabalhando com textos

--- criando-aplicação/screen-match.php
+++ criando-aplicação/screen-match.php
@@ -1,7 +1,7 @@
 <?php
 
-echo "Bem vindo ao Screen Match!
-";
+// "\n" faz uma quebra de linha na string e "\t" faz uma tab na string
+echo "Bem vindo ao Screen Match!";
 
 $nomeFilme = "Top Gun - Maverick";
 $anoLancamento = 2022;
@@ -14,4 +14,13 @@ $planoPrime = false;
 $incluidoNoPlano = $planoPrime || $anoLancamento < 2020;
 
 
-echo $notaFilme;
+// usar aspas duplas em php, ele tenta interpretar o que há dentro dela
+// como no caso da variável $notaFilme dentro da string
+// e no caso do \n 
+
+// aspas simples não permite isso, só imprime o que vai estar lá dentro
+echo "Nome do filme: " . $nomeFilme . "\n"; 
+echo "Nota do filme $notaFilme \n";
+
+// dessa forma abaixo, aí já seria possível
+echo 'texto' . "\n ";
