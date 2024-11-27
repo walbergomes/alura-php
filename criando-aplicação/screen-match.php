49e56f68d8188144036c55d2405abcc5bc65b8c2
Walber <walberg.s48@gmail.com>
2024-11-27 15:22:12 -0300

    entrada de dados

--- criando-aplicação/screen-match.php
+++ criando-aplicação/screen-match.php
@@ -4,7 +4,13 @@
 echo "Bem vindo ao Screen Match!";
 
 $nomeFilme = "Top Gun - Maverick";
-$anoLancamento = 2022;
+
+// a variável $argv é responsável por pegar os dados enviados via terminal
+// inclusive o nome do aquivo que está sendo executado: "php screen-match.php"
+// use o índice da posição do dao que você quer usar
+
+//php screen-match.php 2023 -> 2023 é índice 1
+$anoLancamento = $argv[1] ?? 2022;
 
 $somaDeNotas = 8.8 + 9 + 4 + 7 + 7.7;
 $notaFilme = $somaDeNotas / 5;
@@ -21,6 +27,4 @@ $incluidoNoPlano = $planoPrime || $anoLancamento < 2020;
 // aspas simples não permite isso, só imprime o que vai estar lá dentro
 echo "Nome do filme: " . $nomeFilme . "\n"; 
 echo "Nota do filme $notaFilme \n";
-
-// dessa forma abaixo, aí já seria possível
-echo 'texto' . "\n ";
+echo "Ano de lançamento $anoLancamento \n"; 
