9529bd8429e0ef4cad0d91ea445679bb70a5bc33
Walber <walberg.s48@gmail.com>
2024-11-27 16:14:42 -0300

    expressao condicional - match

--- criando-aplicação/screen-match.php
+++ criando-aplicação/screen-match.php
@@ -3,6 +3,8 @@
 echo "Bem vindo ao Screen Match!";
 
 $nomeFilme = "Top Gun - Maverick";
+$nomeFilme = "Thor Ragnarock";
+$nomeFilme = "Se beber não case";
 
 $anoLancamento = $argv[1] ?? 2022;
 
@@ -16,3 +18,13 @@ $incluidoNoPlano = $planoPrime || $anoLancamento < 2020;
 echo "Nome do filme: " . $nomeFilme . "\n"; 
 echo "Nota do filme $notaFilme \n";
 echo "Ano de lançamento $anoLancamento \n"; 
+
+// match é exclusivo do php
+$genero = match($nomeFilme) {
+  "Top Gun - Maverick" => "ação",
+  "Se beber não case" => "comédia",
+  "Thor Ragnarock" => "super-herói",
+  default => "gênero desconhecido",
+};
+
+echo "O gênero do filme é: $genero";
