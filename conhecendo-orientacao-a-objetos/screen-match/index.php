ff4637ea3df1840a5d89c233984f1bfa26f5aae9
Walber <walberg.s48@gmail.com>
2025-02-20 17:59:31 -0300

    definindo acoes para o filme

---
+++ conhecendo-orientacao-a-objetos/screen-match/index.php
@@ -0,0 +1,19 @@
+<?php
+
+require __DIR__ . "/src/Modelo/Filme.php";
+
+echo "Bem vindo ao ScreenMatch\n";
+
+$filme = new Filme();
+$filme->nome = "Thor - Ragnarok";
+$filme->anoLancamento = 2022;
+$filme->genero = "super-heroi";
+
+$filme->avalia(10);
+$filme->avalia(7);
+$filme->avalia(6.5);
+$filme->avalia(7.2);
+
+var_dump($filme);
+
+echo $filme->media();
---
+++ conhecendo-orientacao-a-objetos/screen-match/src/Modelo/Filme.php
@@ -0,0 +1,19 @@
+<?php
+
+class Filme {
+  public string $nome;
+  public int $anoLancamento;
+  public string $genero;
+  public array $notas = [];
+
+  function avalia(float $nota) : void {
+    $this->notas[] = $nota;
+  }
+
+  function media() : float {
+    $somaNotas = array_sum($this->notas);
+    $quantidadeNotas = count($this->notas);
+
+    return $somaNotas / $quantidadeNotas;
+  }
+}
