4db017dcb55e200afac9ff5f1f2c742e077d63b8
Walber <walberg.s48@gmail.com>
2025-02-21 18:14:03 -0300

    calculadora de maratona - serie

--- conhecendo-orientacao-a-objetos/screen-match/src/Modelo/Serie.php
+++ conhecendo-orientacao-a-objetos/screen-match/src/Modelo/Serie.php
@@ -13,7 +13,7 @@ class Serie extends Titulo
     parent::__construct($nome, $anoLancamento, $genero);
   }
 
-  public function duracaoEmMinutos(): int 
+  public function duracaoEmMinutos(): int
   {
     return $this->temporadas * $this->episodiosPorTemporada * $this->minutosPorTemporada;
   }
