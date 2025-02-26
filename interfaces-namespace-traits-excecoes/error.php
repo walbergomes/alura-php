6362f282cf6d94eb8e8198a62af30ad2d3a4b67f
Walber <walberg.s48@gmail.com>
2025-02-26 16:00:46 -0300

    avaliacao negativa

--- interfaces-namespace-traits-excecoes/error.php
+++ interfaces-namespace-traits-excecoes/error.php
@@ -10,5 +10,13 @@ require 'autoload.php';
 $serie = new Serie('Nome da serie', 2024, Genero::Acao, 7, 20, 30);
 $episodio = new Episodio($serie, 'Piloto', 1);
 
-$conversor = new ConversorNotaEstrela();
-echo $conversor->converte($episodio);
+try {
+    $episodio->avalia(10);
+    $episodio->avalia(-10);
+    
+    
+    $conversor = new ConversorNotaEstrela();
+    echo $conversor->converte($episodio);
+} catch (Exception $e) {
+    echo "Um problema aconteceu: " . $e->getMessage();
+}
--- interfaces-namespace-traits-excecoes/src/Modelo/ComAvaliacao.php
+++ interfaces-namespace-traits-excecoes/src/Modelo/ComAvaliacao.php
@@ -6,8 +6,15 @@ trait ComAvaliacao
 {
     private array $notas = [];
 
+    /**
+     * @throws \InvalidArgumentException Se a nota for negativa ou menor do que 10
+     */
     public function avalia(float $nota): void
     {
+        if ($nota < 0 || $nota > 10) {
+            throw new \InvalidArgumentException('Nota precisa ser entre 0 e 10');
+        }
+
         $this->notas[] = $nota;
     }
 
