f533a5d2dfd9176c27222c16735cd2f57d3e0628
Walber <walberg.s48@gmail.com>
2025-02-27 15:04:15 -0300

    fazendo busca

--- composer-dependencias-autoload-publicacoes/bucar-cursos.php
+++ composer-dependencias-autoload-publicacoes/bucar-cursos.php
@@ -1,8 +1,20 @@
 <?php
 
+require 'vendor/autoload.php';
+
 use GuzzleHttp\Client;
+use Symfony\Component\DomCrawler\Crawler;
 
 $client = new Client();
-$response = $client->request(method: 'GET', uri: 'https://cursos.alura.com.br/category/programacao/php');
+$response = $client->request(method: 'GET', uri: 'https://www.cnnbrasil.com.br/');
 
 $html = $response->getBody();
+
+$crawler = new Crawler();
+$crawler->addHtmlContent($html);
+
+$cursos = $crawler->filter(selector: 'h3.block__news__title');
+
+foreach ($cursos as $curso) {
+    echo $curso->textContent . PHP_EOL;
+}
