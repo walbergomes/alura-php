2d6d687af0d6c89f416d6e60e4752040eabb8dac
Walber <walberg.s48@gmail.com>
2025-03-06 17:07:03 -0300

    Prepared statements

--- pdo-trabalhando-banco-de-dados/inserir-aluno.php
+++ pdo-trabalhando-banco-de-dados/inserir-aluno.php
@@ -7,8 +7,17 @@ require_once 'vendor/autoload.php';
 $databasePath = __DIR__ . '/banco.sqlite';
 $pdo = new PDO(dsn: 'sqlite:' . $databasePath);
 
-$student = new Student(null, 'Walber', new \DateTimeImmutable('2002-01-01'));
+$student = new Student(
+    null,
+    "Walber' ''); DROP TABLE students; -- Gomes",
+    new \DateTimeImmutable('2002-01-01')
+);
 
-$sqlInset = "INSERT INTO students (name, birth_date) VALUES ('{$student->name()}', '{$student->birthDate()->format('Y-m-d')}');";
+$sqlInset = "INSERT INTO students (name, birth_date) VALUES (?, ?);";
+$statement = $pdo->prepare($sqlInset);
+$statement->bindValue(1, $student->name());
+$statement->bindValue(2, $student->birthDate()->format('Y-m-d'));
 
-var_dump($pdo->exec($sqlInset));
+if ($statement->execute()) {
+    echo "Aluno incluído";
+};
